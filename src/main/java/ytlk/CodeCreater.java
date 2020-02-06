ee2dd49e2baedaf9892e9405a7a0ac00ef048329
Administrator <Administrator@192.168.0.103>
2020-02-06 22:15:30 +0800

    1

--- src/main/java/ytlk/CodeCreater.java
+++ src/main/java/ytlk/CodeCreater.java
@@ -12,10 +12,10 @@ import utils.TempUtil;
 
 public class CodeCreater {
 	public static final String MODEL_NAME = "supply";
-//	public static final String sourceFile = "C:\\Users\\Administrator\\git\\mhyUtils\\src\\main\\java\\ytlk\\source\\mm\\contact";//台式机
-	public static final String sourceFile = "F:\\biancheng\\workSpaces\\mhyUtils\\mhyUtils\\src\\main\\java\\ytlk\\source\\wuzi\\supplyType";//笔记本
-//	public static final String templatePathSufix = "";
-	public static final String templatePathSufix = "/tree";//树结构专用
+	public static final String sourceFile = "C:\\Users\\Administrator\\git\\mhyUtils\\src\\main\\java\\ytlk\\source\\wuzi\\supplies";//台式机
+//	public static final String sourceFile = "F:\\biancheng\\workSpaces\\mhyUtils\\mhyUtils\\src\\main\\java\\ytlk\\source\\wuzi\\supplies";//笔记本
+	public static final String templatePathSufix = "";
+//	public static final String templatePathSufix = "/tree";//树结构专用
 	//包名，如果不写则没
 	public static final String PACKAGE_NAME = "";
 	public static final String baseOutPath="C:\\\\Users\\\\Administrator\\\\Desktop\\ytlkCode\\";
