4aad4657a66413d1725f70b4cb65a42cf34318a1
Administrator <Administrator@CN-20191127WLVX>
2020-02-06 19:15:21 +0800

    w

--- src/main/java/crawler/webmagic/gov/hsq/AhZf.java
+++ src/main/java/crawler/webmagic/gov/hsq/AhZf.java
@@ -23,7 +23,6 @@ public class AhZf implements PageProcessor {
     @Override
     public void process(Page page) {
     	page.putField(SpiderConst.SITE_NAME_KEY,SITE_NAME);
-    	System.out.println(page.getHtml().toString());
     	List<Selectable> slList = page.getHtml().xpath("//div[@class='w100']/ul/li").nodes();
     	
     	List<InfoEntity> infoList = new ArrayList<InfoEntity>();
@@ -33,7 +32,6 @@ public class AhZf implements PageProcessor {
     		info.setTitle(sl.xpath("//a/text()").get());
     		info.setDate(sl.xpath("//span/text()").get().replace("[","").replace("]", ""));//[2020-02-03]
     		info.setHref(SITE_BASE_PATH + sl.xpath("//a/@href").get());
-    		System.out.println(info.getDate()+".............");
     		if(MyFilter.hasKeyWord(info.getTitle())&&MyFilter.dateCond(info.getDate())){
     			infoList.add(info);
     		}
@@ -56,10 +54,10 @@ public class AhZf implements PageProcessor {
     			.addUrl("http://ahq.gov.cn/news/town/")//乡镇动态
     			.thread(1);
 //    	System.setProperty("selenuim_config",ClassLoader.getSystemResource("")+"config.ini");
-    	System.setProperty("selenuim_config","C:\\Users\\Administrator\\git\\mhyUtils\\target\\classes\\config.ini");
+    	System.setProperty("selenuim_config","F:\\biancheng\\workSpaces\\mhyUtils\\mhyUtils\\src\\main\\resources\\config.ini");
 //    	spider.setDownloader(new MyHttpClientDownloader());
 //    	spider.setDownloader(new SeleniumDownloader(ClassLoader.getSystemResource("")+"chromedriver.exe"));//模拟浏览器
-    	spider.setDownloader(new SeleniumDownloader("C:\\Users\\Administrator\\git\\mhyUtils\\target\\classes\\chromedriver.exe"));//模拟浏览器
+    	spider.setDownloader(new SeleniumDownloader("C:\\Users\\Administrator\\Desktop\\chromedriver.exe"));//模拟浏览器
     	spider.addPipeline(new ExcelPipleLine());
     	spider.run();
     	
--- src/main/java/ytlk/CodeCreater.java
+++ src/main/java/ytlk/CodeCreater.java
@@ -11,13 +11,13 @@ import utils.StringUtil;
 import utils.TempUtil;
 
 public class CodeCreater {
-	public static final String MODEL_NAME = "mm";
-	public static final String sourceFile = "C:\\Users\\Administrator\\git\\mhyUtils\\src\\main\\java\\ytlk\\source\\mm\\contact";//台式机
-//	public static final String sourceFile = "F:\\biancheng\\workSpaces\\mhyUtils\\mhyUtils\\src\\main\\java\\ytlk\\source\\cms\\info";//笔记本
-	public static final String templatePathSufix = "";
-//	public static final String templatePathSufix = "/tree";//树结构专用
+	public static final String MODEL_NAME = "supply";
+//	public static final String sourceFile = "C:\\Users\\Administrator\\git\\mhyUtils\\src\\main\\java\\ytlk\\source\\mm\\contact";//台式机
+	public static final String sourceFile = "F:\\biancheng\\workSpaces\\mhyUtils\\mhyUtils\\src\\main\\java\\ytlk\\source\\wuzi\\supplyType";//笔记本
+//	public static final String templatePathSufix = "";
+	public static final String templatePathSufix = "/tree";//树结构专用
 	//包名，如果不写则没有
-	public static final String PACKAGE_NAME = null;
+	public static final String PACKAGE_NAME = "";
 	public static final String baseOutPath="C:\\\\Users\\\\Administrator\\\\Desktop\\ytlkCode\\";
 //	public static final String baseOutPath="C:\\Users\\ab048704\\Desktop\\ytlkCode\\";
 	public static void main(String[] args) {
